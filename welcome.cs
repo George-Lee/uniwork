a562679bec9e95ea44f02f1924932ab877bbcbac
George Lee <leegeorg07@gmail.com>
2015-09-23 14:17:51 +0100

    yay hello world!

---
+++ welcome.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Welcome
+{
+	static void Main()
+	{
+		Console.WriteLine("Hello, World!");
+		Console.WriteLine("This is a shitty hello world program");
+		Console.WriteLine("And I cannot type for shit right now!");
+	}
+}
