2a2dfc575c412a8f46753f48957e5821bc6dcd2f
George Lee <leegeorg07@gmail.com>
2015-09-25 09:15:50 +0100

    Dem bugs doe

--- maths.cs
+++ maths.cs
@@ -5,11 +5,11 @@ class Maths
 	static void Main()
 	{
 	    Console.WriteLine("Enter shit: ");
-	    string test = Console.Read();
-		Console.WriteLine("You entered ", test, " what number do you want to say?");
-		test = Console.Readline();
-		console.WriteLine(int.Parse(test));
-		Console.WriteLine("4 * 5 = " + 4*5);
-		Console.WriteLine("This is a test of pushing on the shitty uni network");
+	    string test = Console.ReadLine();
+	    Console.WriteLine("You entered ", test, " what number do you want to say?");
+	    test = Console.ReadLine();
+	    Console.WriteLine(int.Parse(test));
+	    Console.WriteLine("4 * 5 = " + 4*5);
+	    Console.WriteLine("This is a test of pushing on the shitty uni network");
 	}
 }
