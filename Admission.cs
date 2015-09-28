b64ae0663f7a9837444d58dc70f7fb3644183cb4
George Lee <leegeorg07@googlemail.com>
2015-09-28 16:19:11 +0100

    Making a shitty admission thing

---
+++ Admission.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Admission
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your age in years (as an integer)?");
+        string age = Console.Readline();
+        
+        Console.WriteLine("Your age is " + age);
+    }
+}
