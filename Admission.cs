a32ef1fce55749a90ab4d6930186fd6e1f058977
George Lee <leegeorg07@googlemail.com>
2015-09-28 16:22:20 +0100

    Initial conditional admission.

--- Admission.cs
+++ Admission.cs
@@ -7,6 +7,13 @@ class Admission
         Console.WriteLine("Enter your age in years (as an integer)?");
         string age = Console.Readline();
         
-        Console.WriteLine("Your age is " + age);
+        int ageInt = int.Parse(age);
+        
+        Console.WriteLine("Your age is " + ageInt);
+        
+        if (ageInt>=18)
+        {
+            Console.WriteLine("You may come to uni!");
+        }
     }
 }
